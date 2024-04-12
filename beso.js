084ddb6f3519b807cc44e6ea3cb623dac27ff1b7
Omar Villamizar <omarjesusvi@ufps.edu.co>
2024-04-11 19:48:26 -0500

    as

--- beso.js
+++ beso.js
@@ -1,10 +1,8 @@
-let contador = 0; // Inicializamos el contador en 0
+let contador = 0; 
 
-// Función que se ejecuta al hacer clic en el botón
 function sumarBeso() {
-    contador++; // Incrementamos el contador en 1
-    document.querySelector('label').innerText = 'Besos que te debo: ' + contador; // Actualizamos el texto del label con el contador
+    contador++; 
+    document.querySelector('label').innerText = 'Besos que te debo: ' + contador; 
 }
 
-// Obtenemos el botón y le añadimos un event listener para que llame a la función sumarBeso() cuando se haga clic
 document.querySelector('button').addEventListener('click', sumarBeso);
